dcf0997afadba401d0eb3ffb8f68b2bc34d6e3d4
Elijah Mejilla <111504836+jgmejilla@users.noreply.github.com>
2025-06-14 03:11:36 +0800

    fetch articles to frontend

---
+++ app/src/routes/feed/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    let { data } = $props()
+    console.log(data)
+</script>
+
+<ul>
+    {#each data.articles as article}
+    <li>
+        {article.title}; retrieved on {article.time_scraped}
+    </li>
+    {/each}
+</ul>
